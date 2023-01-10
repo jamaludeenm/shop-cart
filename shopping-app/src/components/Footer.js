9757393e31b955835cff777e13678efc5b21cc19
jamaludeen <justjamal17@gmail.com>
2023-01-10 21:47:13 +0530

    app

--- shopping-app/src/components/Footer.js
+++ shopping-app/src/components/Footer.js
@@ -6,22 +6,22 @@ const Footer = () => {
             <footer id="footer">
                 <div className="container">
                     <a
-                        href="https://github.com/gulshancodes/redux-shopping-cart"
+                        href="https://github.com/jamaludeenm/shop-cart"
                         className="repo_link"
                         target="_blank"
                         rel="noreferrer"
                     >
-                        github.com/gulshancodes/redux-shopping-cart
+                        https://github.com/jamaludeenm/shop-cart
                     </a>
 
                     <p>
                         Built by | &nbsp;
                         <a
-                            href="https://gulshansongara.netlify.app"
+                            href="https://github.com/jamaludeenm"
                             target="_blank"
                             rel="noreferrer"
                         >
-                            Gulshan Songara
+                            Jamal Deen
                         </a>
                     </p>
                 </div>
